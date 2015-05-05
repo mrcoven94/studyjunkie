ff439078c4599ad735fcea453111d3a2ddf3f8fc
Aaron Nech <necha@cs.washington.edu>
2015-05-05 12:09:08 -0700

    initial commit

--- src/client/main.js
+++
@@ -1,4 +0,0 @@
-// browserify.js (our library that clumps together all these javascript files)
-// needs a "main file" to derive all dependencies from and package up.
-// This is that file. 
-//# sourceMappingURL=main.js.map
